634d1c516d5f1ad47563f2338f4faac068ca1354
kiboiWanjohi <sammy313.sw@gmail.com>
2022-07-15 11:56:18 +0300

    CSS

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const btn = document.getElementById("add");
-const tasks = document.getElementById("tasks");
-const input = document.getElementById("input");
-btn.addEventListener("click", printVal);
-function printVal() {
-  const value = input.value;
-  tasks.innerHTML = `<p>${value}</p>`;
+var list = document.getElementById("todolist");
+var addBtn = document.getElementById("btn");
+var addInput = document.getElementById("input");
+var i = document.getElementById("i");
+
+function addToDo() {
+  var text = addInput.value;
+
+  if (text === "") {
+    return;
+  }
+  var li = document.createElement("li");
+  var p = document.createElement("p");
+
+  p.innerHTML = `${text} <i id = "i" class="fa-solid fa-circle-check"></i>`;
+  li.appendChild(p);
+  list.appendChild(li);
+  text = "";
+}
+addBtn.addEventListener("click", addToDo);
+addInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addToDo();
+  }
+});
+list.addEventListener("click", function (event) {
+  removeToDo(event.target);
+});
+function removeToDo(removeElement) {
+  removeElement.parentElement.remove();
 }
